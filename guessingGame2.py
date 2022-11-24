9787543f3da89415d1c49c8d83cfcfecfdb54d93
BadBoioNo1 <bucurgabriel25@yahoo.com>
2022-11-24 17:31:32 +0200

    Learned functions

--- guessingGame2.py
+++ guessingGame2.py
@@ -1,16 +1,37 @@
 import random
 
+
+def get_integer(prompt):
+    """
+    Get an integer from Standard Input (stdin).
+
+    The function will continue looping and prompting
+    the user, until a valid `int` is entered.
+
+    :param prompt: The String that the user will see, when
+        they're prompted to enter the value.
+    :return: The integer that the user enters.
+    """
+    while True:
+        temp = input(prompt)
+        if temp.isnumeric():
+            return int(temp)
+        # else:
+        print("'{}' is not a valid number! Try again".format(temp))     # else not needed
+
+
 highest = 10
 answer = random.randint(1, highest)
 print(answer)
-guess = int(input("Please guess number between 1 and {}: ".format(highest)))
+guess = 0
+print("Please guess number between 1 and {}".format(highest), end=' ')
 
 while guess != answer:
+    guess = get_integer(": ")
     if guess > answer:
         print("Guess lower")
     else:
         print("Guess higher")
-    guess = int(input())
 if guess == answer:
     print("Well done, you got it")
 
