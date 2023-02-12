5e42590f1e843bda0bf206d60524750b1f826200
BadBoioNo1 <bucurgabriel25@yahoo.com>
2023-02-12 13:32:49 +0200

    Learned OOP

---
+++ OOP/main.py
@@ -0,0 +1,13 @@
+from player import Player
+
+tim = Player("Tim")
+
+# print(tim.name)
+# print(tim.lives)
+tim.lives -= 1
+print(tim)
+tim.level += 1
+print(tim)
+tim.lives -= 1
+tim.level += 1
+print(tim)
